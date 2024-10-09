dfd3e9e10cf0366f753dd78f594e35a769b9a16d
Leander Pyrz <konsti@niklascfw.de>
2024-10-09 13:56:35 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -109,11 +109,8 @@ export default function Component() {
       <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t bg-white">
         <p className="text-xs text-gray-500">© 2024 Wavy Bot. All rights reserved.</p>
         <nav className="sm:ml-auto flex gap-4 sm:gap-6">
-          <a className="text-xs hover:underline underline-offset-4 text-gray-500" href="#">
-            Terms of Service
-          </a>
-          <a className="text-xs hover:underline underline-offset-4 text-gray-500" href="#">
-            Privacy
+          <a className="text-xs hover:underline underline-offset-4 text-gray-500" href="https://legal.thunderlink.xyz/">
+            Legal Informations
           </a>
         </nav>
       </footer>
