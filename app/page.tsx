549d033649f822699e621ffbf8f7df357e380fab
Leander Schlichting <konsti@niklascfw.de>
2024-10-13 13:48:12 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -110,7 +110,7 @@ export default function Component() {
       <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t bg-white dark:bg-gray-900 dark:border-gray-700">
         <p className="text-xs text-gray-500 dark:text-gray-400">© 2024 Wavy Bot. All rights reserved.</p>
         <nav className="sm:ml-auto flex gap-4 sm:gap-6">
-          <a className="text-xs hover:underline underline-offset-4 text-gray-500 dark:text-gray-400" href="https://legal.thunderlink.xyz/">
+          <a className="text-xs hover:underline underline-offset-4 text-gray-500 dark:text-gray-400" href="https://legal.wavybot.me/">
             Legal Informations
           </a>
         </nav>
